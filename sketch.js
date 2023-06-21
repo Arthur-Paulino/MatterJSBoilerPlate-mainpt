c44045f7792f5625718503b1b3dc90190f6ff8f4
Arthur-Paulino <128192898+Arthur-Paulino@users.noreply.github.com>
2023-06-20 21:49:29 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,9 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+var engine, world;
 
+var block1,block2,block3;
 function preload()
 {
 	
@@ -17,7 +19,43 @@ function setup() {
 	world = engine.world;
 
 	//Crie os Corpos Aqui.
+var plane_options={
+      isStatic: true
+    }
 
+    plane = Bodies.rectangle(600,580,1200,2,plane_options);
+    World.add(world,plane);
+  
+    //criados múltiplos corpos de blocos 
+    var block1_options = {
+      restitution:0.5,
+      friction:0.02,
+      frictionAir:0
+    }
+
+    var block2_options = {
+      restitution:0.7,
+      friction:0.01,
+      frictionAir:0.1
+    }
+
+    var block3_options = {
+      restitution:0.01,
+      friction:1,
+      frictionAir:0.3
+    }
+
+    block1 = Bodies.circle(220,10,10,block1_options);
+    World.add(world,block1);
+    
+    block2 = Bodies.rectangle(110,50,10,10,block2_options);
+    World.add(world,block2);
+    
+    block3 = Bodies.rectangle(350,50,10,10,block3_options);
+    World.add(world,block3);
+    
+    
+    //esti…
 
 	Engine.run(engine);
   
@@ -25,9 +63,18 @@ function setup() {
 
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
+ 
+ background("lightgreen");
+    Engine.update(engine);
+
+  //criada forma para o plano e suporte
+  rect(plane.position.x,plane.position.y,1200);
   
+
+  //criadas formas para todas as partículas
+  ellipse(block1.position.x,block1.position.y,30);
+  rect(block2.position.x,block2.position.y,50,50)
+  rect(block3.position.x,block3.position.y,100,50) 
   drawSprites();
  
 }
